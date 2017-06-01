141275c50e9f0a698c7cf5209f7e73ef1244b8c3
Dmitriy Pushkov <ezze@ezze.org>
2017-06-01 23:15:42 +0300

    Bower is replaced by NPM, Grunt is replaced by Webpack.

--- src/js/index.js
+++ src/js/index.js
@@ -1,140 +1,144 @@
-(function($) {
-    var enableParallax = true;
+import $ from 'jquery';
+import 'bootstrap';
+import 'jquery.stellar';
+import 'jquery-form-validator';
 
-    if (enableParallax) {
-        $(window).on('load', function() {
-            $('body').removeClass('loading');
+import '../less/kitgeo.less';
 
-            $.stellar({
-                horizontalScrolling: false
-            });
-        }).on('resize', function() {
-            $.stellar('refresh');
+const enableParallax = false;
+
+if (enableParallax) {
+    $(window).on('load', function() {
+        $('body').removeClass('loading');
+        $.stellar({
+            horizontalScrolling: false
         });
+    }).on('resize', function() {
+        $.stellar('refresh');
+    });
+}
+
+$(function() {
+    if (!enableParallax) {
+        $('body').removeClass('loading');
+        $('[data-stellar-background-ratio]').css('backgroundAttachment', 'scroll');
     }
 
-    $(function() {
-        if (!enableParallax) {
-            $('body').removeClass('loading');
-            $('[data-stellar-background-ratio]').css('backgroundAttachment', 'scroll');
+    // Navbar anchors
+    $('#main-navbar').on('click', 'a', function(event) {
+        const href = $(this).attr('href');
+        if (!href.match(/^#/) && !$(this).hasClass('navbar-brand')) {
+            return;
         }
 
-        // Navbar anchors
-        $('#main-navbar').on('click', 'a', function(event) {
-            var href = $(this).attr('href');
-            if (!href.match(/^#/) && !$(this).hasClass('navbar-brand')) {
-                return;
+        const scrollTop = $(this).hasClass('navbar-brand') ? 0 : $(href).offset().top;
+        const $navbarCollapse = $(this).parents('.navbar-collapse');
+        if ($navbarCollapse.hasClass('in') || $navbarCollapse.hasClass('collapsing')) {
+            $navbarCollapse.collapse('hide');
+        }
+
+        event.preventDefault();
+        $('body,html').animate({
+            'scrollTop': scrollTop
+        }, {
+            duration: 500,
+            queue: false,
+            complete: function() {
+                location.hash = href;
             }
-
-            var scrollTop = $(this).hasClass('navbar-brand') ? 0 : $(href).offset().top;
-            var $navbarCollapse = $(this).parents('.navbar-collapse');
-            if ($navbarCollapse.hasClass('in') || $navbarCollapse.hasClass('collapsing')) {
-                $navbarCollapse.collapse('hide');
-            }
-
-            event.preventDefault();
-            $('body,html').animate({
-                'scrollTop': scrollTop
-            }, {
-                duration: 500,
-                queue: false,
-                complete: function() {
-                    location.hash = href;
-                }
-            });
-        });
-
-        // Feedback form
-        var validationResult = false;
-        var submitting = false;
-
-        var $feedbackForm = $('.feedback-form');
-
-        $.validate({
-            modules: 'security',
-            onValidate: function() {
-                $feedbackForm.find('.form-submit-error').remove();
-            },
-            onSuccess: function() {
-                validationResult = true;
-            },
-            onError: function() {
-                validationResult = false;
-            }
-        });
-
-        $feedbackForm.on('submit', function(event) {
-            event.preventDefault();
-
-            if (submitting || !validationResult) {
-                return;
-            }
-
-            var url = $(this).attr('action').replace(/\/messages/, '/messages/ajax');
-            var data = {};
-            $.each($(this).serializeArray(), function(index, item) {
-                data[item.name] = item.value;
-            });
-
-            var onSubmitFail = function() {
-                $feedbackForm.find('.form-group:first').before($('<div></div>')
-                    .addClass('form-submit-error')
-                    .addClass('alert')
-                    .addClass('alert-danger')
-                    .addClass('alert-dismissible')
-                    .append($('<button></button>')
-                        .addClass('close')
-                        .attr('type', 'button')
-                        .attr('data-dismiss', 'alert')
-                        .attr('aria-label', 'Закрыть')
-                        .append($('<span></span>')
-                            .attr('aria-hidden', 'true')
-                            .html('&times;')
-                        )
-                    )
-                    .append($('<span></span>')
-                        .text('Не удалось отправить форму. Попробуйте повторить попытке позднее.')
-                    )
-                )
-            };
-
-            $.ajax({
-                dataType: 'jsonp',
-                url: url,
-                data: data,
-                beforeSend: function() {
-                    submitting = true;
-                }
-            }).done(function(data) {
-                if (!data.success) {
-                    onSubmitFail();
-                    return;
-                }
-
-                $feedbackForm.replaceWith($('<div></div>')
-                    .addClass('form-submit-success')
-                    .addClass('alert')
-                    .addClass('alert-success')
-                    .addClass('alert-dismissible')
-                    .append($('<button></button>')
-                        .addClass('close')
-                        .attr('type', 'button')
-                        .attr('data-dismiss', 'alert')
-                        .attr('aria-label', 'Закрыть')
-                        .append($('<span></span>')
-                            .attr('aria-hidden', 'true')
-                            .html('&times;')
-                        )
-                    )
-                    .append($('<span></span>')
-                        .text('Большое спасибо за Ваше сообщение. Мы свяжемся с Вами в ближайшее время.')
-                    )
-                );
-            }).fail(function() {
-                onSubmitFail();
-            }).always(function() {
-                submitting = false;
-            });
         });
     });
-})(jQuery);
+
+    // Feedback form
+    let validationResult = false;
+    let submitting = false;
+
+    const $feedbackForm = $('.feedback-form');
+
+    $.validate({
+        modules: 'security',
+        onValidate: function() {
+            $feedbackForm.find('.form-submit-error').remove();
+        },
+        onSuccess: function() {
+            validationResult = true;
+        },
+        onError: function() {
+            validationResult = false;
+        }
+    });
+
+    $feedbackForm.on('submit', function(event) {
+        event.preventDefault();
+
+        if (submitting || !validationResult) {
+            return;
+        }
+
+        const url = $(this).attr('action').replace(/\/messages/, '/messages/ajax');
+        const data = {};
+        $.each($(this).serializeArray(), function(index, item) {
+            data[item.name] = item.value;
+        });
+
+        var onSubmitFail = function() {
+            $feedbackForm.find('.form-group:first').before($('<div></div>')
+                .addClass('form-submit-error')
+                .addClass('alert')
+                .addClass('alert-danger')
+                .addClass('alert-dismissible')
+                .append($('<button></button>')
+                    .addClass('close')
+                    .attr('type', 'button')
+                    .attr('data-dismiss', 'alert')
+                    .attr('aria-label', 'Закрыть')
+                    .append($('<span></span>')
+                        .attr('aria-hidden', 'true')
+                        .html('&times;')
+                    )
+                )
+                .append($('<span></span>')
+                    .text('Не удалось отправить форму. Попробуйте повторить попытке позднее.')
+                )
+            )
+        };
+
+        $.ajax({
+            dataType: 'jsonp',
+            url: url,
+            data: data,
+            beforeSend: function() {
+                submitting = true;
+            }
+        }).done(function(data) {
+            if (!data.success) {
+                onSubmitFail();
+                return;
+            }
+
+            $feedbackForm.replaceWith($('<div></div>')
+                .addClass('form-submit-success')
+                .addClass('alert')
+                .addClass('alert-success')
+                .addClass('alert-dismissible')
+                .append($('<button></button>')
+                    .addClass('close')
+                    .attr('type', 'button')
+                    .attr('data-dismiss', 'alert')
+                    .attr('aria-label', 'Закрыть')
+                    .append($('<span></span>')
+                        .attr('aria-hidden', 'true')
+                        .html('&times;')
+                    )
+                )
+                .append($('<span></span>')
+                    .text('Большое спасибо за Ваше сообщение. Мы свяжемся с Вами в ближайшее время.')
+                )
+            );
+        }).fail(function() {
+            onSubmitFail();
+        }).always(function() {
+            submitting = false;
+        });
+    });
+});
---
+++ webpack.config.babel.js
@@ -0,0 +1,133 @@
+import path from 'path';
+import webpack from 'webpack';
+
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+import packageJson from './package.json';
+
+const NODE_ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+const extractLess = new ExtractTextPlugin({
+    filename: 'css/[name].css',
+    disable: false,
+    allChunks: true
+});
+
+const config = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        index: './js/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'assets'),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            use: 'babel-loader',
+            include: [
+                path.resolve(__dirname, 'src')
+            ]
+        }, {
+            test: /\.less$/,
+            use: extractLess.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader",
+                publicPath: '../'
+            }),
+            include: [
+                path.resolve(__dirname, 'src')
+            ]
+        }, {
+            test: /\.(jpg|png|gif)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: 'css-img/[name].[hash:6].[ext]'
+                }
+            },
+            include: [
+                path.resolve(__dirname, 'src')
+            ]
+        }, {
+            test: /\.(eot|otf|svg|ttf|woff2?)/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: '[1].[hash:6].[ext]',
+                    regExp: 'src/(.+)\\.[^.]+'
+                }
+            },
+            include: [
+                path.resolve(__dirname, 'src')
+            ]
+        }, {
+            test: /\.(eot|otf|svg|ttf|woff2?)/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: '[1].[hash:6].[ext]',
+                    regExp: 'node_modules/bootstrap/(.+)\\.[^.]+'
+                }
+            },
+            include: [
+                path.resolve(__dirname, 'node_modules/bootstrap')
+            ]
+        }]
+    },
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.jsx'],
+        alias: {
+            'jquery.stellar': 'jquery.stellar/jquery.stellar'
+        }
+    },
+    resolveLoader: {
+        modules: ['node_modules'],
+        moduleExtensions: ['.js']
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+            NODE_ENV: JSON.stringify(NODE_ENV),
+            VERSION: JSON.stringify(packageJson.version)
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        extractLess,
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: module => {
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest',
+            minChunks: Infinity
+        })
+    ],
+    devtool: NODE_ENV === 'development' ? 'source-map' : false
+};
+
+if (NODE_ENV === 'production') {
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        comments: false,
+        compress: {
+            warnings: false,
+            drop_console: false,
+            unsafe: false
+        },
+        sourceMap: false
+    }));
+}
+
+export default config;
